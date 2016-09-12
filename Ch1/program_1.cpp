b1db6bdbe8619ce6bcfbf9011dd23c1bc67529d2
Humdaan Mustafa <humishum@gmail.com>
2016-09-12 11:45:04 -0700

    program_1

---
+++ Ch1/program_1.cpp
@@ -0,0 +1,13 @@
+// Humdaan Mustafa
+// cs-102, period 4, Fall 2016
+//Program_1
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Welcome to CS-102, Fall 2016, period 4!" << endl;
+  cout <<"Hello, World!" << endl;
+	return 0;
+}
